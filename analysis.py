626f44d8c7130b39687acd33565f45facd0774fd
Colm <colmocaoimh@outlook.com>
2020-04-11 06:25:48 +0100

    Summary; more efficient code

--- analysis.py
+++ analysis.py
@@ -1,32 +1,29 @@
+# Colm O Caoimh
+# PandS project 2020
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 iris_data = np.genfromtxt("iris.data", delimiter=',')
 
-#print(iris_data[:,0])
+# Code from Wikipedia 'Five Number Summary' page
+def fivenum(data):
+    return np.percentile(data, [0, 25, 50, 75, 100], interpolation='midpoint')
 
-sepal_length = iris_data[:,0]
+# Create dictionary of variable data for use in for loop
+measurement_dict = {
+    "sepal_length": iris_data[:,0],
+    "sepal_width": iris_data[:,1],
+    "petal_length": iris_data[:,2],
+    "petal_width": iris_data[:,3],
+}
 
-# mean (rounded to 3 decimal places)
-mean = round(np.mean(sepal_length), 3)
-print("Sepal length mean: ", mean)
 
-# max lingth
-max_length = max(sepal_length)
-print("Sepal length max: ", max_length)
+# for loop 
+for measurement in measurement_dict:
+    print(fivenum(measurement_dict[measurement]))
+    
 
-# min length
-min_length = min(sepal_length)
-print("Sepal length min: ", min_length)
-
-# 1st quartile
-first_quartile = np.percentile(sepal_length, 25)
-print(first_quartile)
-
-# 3rd quartile
-third_quartile = np.percentile(sepal_length, 75)
-print(third_quartile)
-
-# median
-median = np.median(sepal_length)
-print(median)
+    
