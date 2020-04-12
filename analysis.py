34462b239516ca686058f74d89c3376b21e6d741
Colm <colmocaoimh@outlook.com>
2020-04-12 23:52:36 +0100

    Created dataframe for sumary presentation

--- analysis.py
+++ analysis.py
@@ -8,36 +8,28 @@ import pandas as pd
 # Read file using genfromtxt
 iris_data = np.genfromtxt("iris.data", delimiter=',')
 
-
-# Code from Wikipedia 'Five Number Summary' page
+# Function to calculate 5 number summary for dataset
 def fivenum(data):
-    x = np.percentile(data, [0, 25, 50, 75, 100], interpolation='midpoint')
-    return x
+    summary_points = np.percentile(data, [0, 25, 50, 75, 100], interpolation='midpoint')
+    return summary_points
 
+# Assign variable to each 5 number summary
+sep_length = fivenum(iris_data[:,0])
+sep_width = fivenum(iris_data[:,1])
+pet_length = fivenum(iris_data[:,2])
+pet_width = fivenum(iris_data[:,3])
 
-sepal_length = fivenum(iris_data[:,0])
-sepal_width = fivenum(iris_data[:,1])
-petal_length = fivenum(iris_data[:,2])
-petal_width = fivenum(iris_data[:,3])
+# Combine each array into one 2d array. 
+# Transpose for improved visualisation
+summary_arr = np.vstack((sep_length, sep_width, pet_length, pet_width))
+summary_arr = summary_arr.T
 
-print(petal_length) 
+print(iris_data)
 
-'''
-# Create dictionary of variable data for use in for loop
-measurement_dict = {
-    "sepal_length": iris_data[:,0],
-    "sepal_width": iris_data[:,1],
-    "petal_length": iris_data[:,2],
-    "petal_width": iris_data[:,3],
-}
+# Assign lables for rows and columns of dataframe
+row_names = ['min.', '1st quart.', 'mean', '3rd quart.', 'max.']
+col_names = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width']
 
-
-# for loop outputs summary of data 
-for measurement in measurement_dict:
-    x = fivenum(measurement_dict[measurement])
-
-print(x)
-print(type(x))
- '''   
-
-    
+# Create dataframe for more effective presentation
+summary_df = pd.DataFrame(summary_arr, index=row_names, columns=col_names)
+print(summary_df)
