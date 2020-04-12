62ef3f9a405ba9e8ef31ced50c9c1bbde2bcdd97
Colm <colmocaoimh@outlook.com>
2020-04-12 16:26:49 +0100

    Assign lists of summary points to variables

--- analysis.py
+++ analysis.py
@@ -8,10 +8,21 @@ import pandas as pd
 # Read file using genfromtxt
 iris_data = np.genfromtxt("iris.data", delimiter=',')
 
+
 # Code from Wikipedia 'Five Number Summary' page
 def fivenum(data):
-    return np.percentile(data, [0, 25, 50, 75, 100], interpolation='midpoint')
+    x = np.percentile(data, [0, 25, 50, 75, 100], interpolation='midpoint')
+    return x
 
+
+sepal_length = fivenum(iris_data[:,0])
+sepal_width = fivenum(iris_data[:,1])
+petal_length = fivenum(iris_data[:,2])
+petal_width = fivenum(iris_data[:,3])
+
+print(petal_length) 
+
+'''
 # Create dictionary of variable data for use in for loop
 measurement_dict = {
     "sepal_length": iris_data[:,0],
@@ -23,7 +34,10 @@ measurement_dict = {
 
 # for loop outputs summary of data 
 for measurement in measurement_dict:
-    print(fivenum(measurement_dict[measurement]))
-    
+    x = fivenum(measurement_dict[measurement])
+
+print(x)
+print(type(x))
+ '''   
 
     
