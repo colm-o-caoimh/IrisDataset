9b557d5041c3d77af534ae2da73cb25ee3be0cbb
Colm <colmocaoimh@outlook.com>
2020-04-21 00:26:16 +0100

    Histograms coded and first readme entry

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PAS2020_project
 Final project Programming and Scripting 2020
+
+## Data search and download
+* Following a quick Google search, I established that the Iris data set is used widely
+in the fields of machine learning and statistics. It is referenced in
+numerous academic publications, student projects, online tutorials etc. so there
+was no shortage of sources offering the file for download. The most commonly 
+referenced source appeared to be the UCI (University of California Irvine) website,
+whose machine learning repository offers the data set for download, amongst many others.
+It is this source that I have used in this project.
+
+* It is important to mention that the UCI data set differs slightly from the data 
+presented in Fisher's original paper. This is noted on the website.
+
+## Iris data set
+* The data was collected by Edgar Anderson and introduced by Ronald A. Fisher in his
+paper "The use of multiple measurements in taxonomic problems" in 1936. According
+to the UCI website, the paper "... is a classic in the field [of pattern recognition]
+and is referenced frequently to this day"
+
+* A multivariate data set, it contains a total of 150 instances consisting of 50 instances
+of each of 3 classes: *Iris-setosa*, *Iris-virginica* and *Iris-versicolor*. These refer
+to 3 different species of the iris flower. There are 4 attributes in the sample: *sepal length*,
+*sepal width*, *petal length* and *petal width*, referring to the parts of each flower
+measured by Anderson.
--- analysis.py
+++ analysis.py
@@ -13,7 +13,6 @@ float_values = iris_data.iloc[:,0:4]
 str_values = iris_data.iloc[:,4]
 
 
-'''
 # Use describe() function to summarise data
 float_summary = round(float_values.describe(), 3)
 str_summary = str_values.describe()
@@ -34,7 +33,7 @@ quantity = ['50', '50', '50']
 str_summary['Count'] = quantity
 
 # Rename rows in str_summary
-str_summary.index = ['Type_A', 'Type_B', 'Type_C']
+str_summary.index = ['Species_A', 'Species_B', 'Species_C']
 
 # Write and format summary output to text file
 with open("iris_summary.txt", "w") as f:
@@ -44,13 +43,16 @@ with open("iris_summary.txt", "w") as f:
     f.write(float_summary.to_string() + "\n\n")
     f.write(str_summary.to_string())
 
-'''
 
+
+# create variable for dataset variable
 sep_len = iris_data.iloc[:,0]
 sep_width = iris_data.iloc[:,1]
 pet_len = iris_data.iloc[:,2]
 pet_width = iris_data.iloc[:,3]
+species = iris_data.iloc[:,4]
 
+# write function which outputs a histogram for each dataset variable
 def var_hist(var_data, fig_num, x_label, y_label, title, filepath):
     plt.figure(fig_num)
     plt.xlabel(x_label)
@@ -59,20 +61,16 @@ def var_hist(var_data, fig_num, x_label, y_label, title, filepath):
     plt.hist(var_data, bins=20, rwidth=0.9,)
     plt.savefig(filepath)
 
+def var_hist2(var_data, fig_num, x_label, y_label, title, filepath):
+    plt.figure(fig_num)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.hist(var_data, bins=3, rwidth=0.9)
+    plt.savefig(filepath)
 
 var_hist(sep_len, 1, 'sepal_length_cm', 'count', 'Sepal Length', 'sepal_length.png')
 var_hist(sep_width, 2, 'sepal_width_cm', 'count', 'Sepal Width', 'sepal_width.png')
 var_hist(pet_len, 3, 'petal_length_cm', 'count', 'Petal Length', 'petal_length.png')
 var_hist(pet_width, 4, 'petal_width_cm', 'count', 'Petal Width', 'petal_width.png')
-
-
-'''
-plt.hist(sep_len, rwidth=0.9)
-plt.xlabel('frequency in dataset')
-plt.ylabel('Sepal length') 
-plt.show()
-
-
-
-plt.xlabel('petal_width_cm', size=15)
-'''
+var_hist2(species, 5, 'species', 'count', 'Iris Species', 'species.png')
